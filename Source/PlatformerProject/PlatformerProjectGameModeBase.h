dd0a6180abbd14ef5d697ea2ea527d3e88c908e2
aresProjects <ruipsilva2001@hotmail.com>
2021-11-16 15:25:38 +0000

    Initial commit

---
+++ Source/PlatformerProject/PlatformerProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlatformerProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMERPROJECT_API APlatformerProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
